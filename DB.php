bd3503b36ff63197054dffb79e343fb1abbc4160
igearsChristian <christian@igears.com.hk>
2025-01-13 17:03:12 +0800

    Update DB.php
    separated the init function into DB_init.php

--- DB.php
+++ DB.php
@@ -14,16 +14,8 @@ catch(exception) {
 }
 
 if ($conn) {
-    echo "You are connected! . <br>";
+    // echo "You are connected! . <br>";
 }
 
-$truncate_sql = "TRUNCATE TABLE locations";
-mysqli_query($conn, $truncate_sql);
 
-$inject_sql = "INSERT INTO locations (name, lat, long_)
-VALUES 
-    ('iGears Technology Ltd', 22.357929802706558, 114.13166951186284), 
-    ('Airside Shopping Mall', 22.331685749828214, 114.19804471409547)";
-
-mysqli_query($conn, $inject_sql);
 ?>
