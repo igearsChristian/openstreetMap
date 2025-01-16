54095226cf247c3d2860db8284795658fa48f700
igearsChristian <christian@igears.com.hk>
2025-01-16 12:08:53 +0800

    added navbar

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
       :root {
-          --map-top: 8vh;
+          --map-top: 7vh;
           --control_panel-width: 350px;
           --control_panel-height: 100vh;
       }
@@ -73,10 +73,30 @@
       }
     </style>
 </head>
+<?php 
+    include("header.php");
+?>
 <body>
-<div id="header">
-    <h1>iGears OpenStreetMap</h1>
-</div>
+
+<!-- <div id="header">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img class="logo" src="img/igears3.png" alt="Bootstrap" width="170" height="60"/></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Interactive Map</a></li>
+                <li class="nav-item"><a class="nav-link" href="text_form.php">Database search</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">About us</a></li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
+</div> -->
     <div id="map"></div>
     <div class="leaflet-control info-panel" id="infoPanel">
       <h3>Marker Information</h3>
