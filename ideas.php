88e397cb1e1a9452a607dfbc1148b79403553253
igearsChristian <christian@igears.com.hk>
2025-01-17 13:54:47 +0800

    info of No ideas

---
+++ ideas.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Location Data</title>
+    <style>
+        .location {
+            margin-bottom: 20px; /* Space between entries */
+        }
+    </style>
+</head>
+<body>
+    <p>try to input:</p>
+
+    <div class="location">
+        <p>Science Park Tech 22.42479463483073 114.21442695013151</p>
+    </div>
+
+    <div class="location">
+        <p>Harbour City Commercial 22.297623441236038 114.16902382282828</p>
+    </div>
+
+    <div class="location">
+        <p>Time Square Commercial 22.27848308723261 114.18168075949441</p>
+    </div>
+</body>
+</html>
