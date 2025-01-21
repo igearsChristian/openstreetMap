ea7c9e2c350654e9db72ab5eeacf65eabc116664
igearsChristian <christian@igears.com.hk>
2025-01-21 15:44:38 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,12 +5,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $category = htmlspecialchars($_POST['category']);
     $lat = htmlspecialchars($_POST['lat']);
     $long = htmlspecialchars($_POST['long']);
+
+    $image = $_FILES['file'];
+    $imagefilename = $image['name'];
+    $imagefiletemp = $image['tmp_name'];
+    $filename_separate = explode('.',$imagefilename);
+    $file_extension = strtolower($filename_separate[1]);
 }
 
 include("DB.php");
 
-$sql = "INSERT INTO locations (name, category, lat, long_)
-        VALUES ('$name','$category','$lat','$long')";
+$upload_image = 'img/'.$imagefilename;
+move_uploaded_file($imagefiletemp,$upload_image);
+
+
+$sql = "INSERT INTO locations (name, category, lat, long_, image)
+        VALUES ('$name','$category','$lat','$long','$upload_image')";
 
 if (mysqli_query($conn, $sql)) {
     header("Location: index.php");
