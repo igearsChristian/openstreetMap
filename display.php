bc585c79abf7310e7d9cc2a91baedb10ec33c629
igearsChristian <christian@igears.com.hk>
2025-01-21 15:44:04 +0800

    added image display in popup

--- display.php
+++ display.php
@@ -3,36 +3,38 @@ $sql = "SELECT * FROM locations";
         $result = mysqli_query($conn, $sql);
         
         if (mysqli_num_rows($result) > 0) {
-            echo "<script>";
 
-            
+          
             while ($row = mysqli_fetch_assoc($result)) {
+                 $image=$row['image'];
+                 echo "<script>";  
                 if ($row['category'] == 'Tech') {
                     echo "
-                    var marker = L.marker([{$row['lat']}, {$row['long_']}], { icon: redIcon }).addTo(IT_group).bindPopup('{$row['name']}');
+                    var marker = L.marker([{$row['lat']}, {$row['long_']}], { icon: redIcon }).addTo(IT_group).bindPopup('<h3>{$row['name']}</h3> <img src=$image style=\"max-width: 100%; height: auto;\">');
                     marker.on('click', function () {
-                        showInfoPanel('{$row['name']}');
+                        showInfoPanel('{$row['name']}','$image');
 
                     });
                 ";
                 } else if ($row['category'] == 'Commerical'){
                     echo "
-                    var marker = L.marker([{$row['lat']}, {$row['long_']}], { icon: blueIcon }).addTo(Commerical_group).bindPopup('{$row['name']}');
+                    var marker = L.marker([{$row['lat']}, {$row['long_']}], { icon: blueIcon }).addTo(Commerical_group).bindPopup('<h3>{$row['name']}</h3> <img src=$image style=\"max-width: 100%; height: auto;\">');
                     marker.on('click', function () {
-                        showInfoPanel('{$row['name']}');
+                        showInfoPanel('{$row['name']}','$image');
                     });
                 ";
                 } else if ($row['category'] == 'Art'){
                     echo "
-                    var marker = L.marker([{$row['lat']}, {$row['long_']}], { icon: artIcon }).addTo(Art_group).bindPopup('<h3>{$row['name']}</h3> <img src=\"./img/artwork.jpg\"style=\"max-width: 100%; height: auto;\">');
+                    var marker = L.marker([{$row['lat']}, {$row['long_']}], { icon: artIcon }).addTo(Art_group).bindPopup('<h3>{$row['name']}</h3> <img src=$image style=\"max-width: 100%; height: auto;\">');
                     marker.on('click', function () {
-                        showInfoPanel('{$row['name']}');
+                        showInfoPanel('{$row['name']}','$image');
                     });
                 ";
                 }
+                echo "</script>";
             }
             
-            echo "</script>";
+            
         } 
         mysqli_close($conn);
 ?>
